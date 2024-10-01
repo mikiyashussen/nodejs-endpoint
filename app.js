ba9200b8e3828b498b62c43b3c4b645e3d5cd9ad
mikiyashussen <mikiyashussen19@gmail.com>
2024-10-01 17:22:47 +0200

    added endpoint for polish version

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to the app!");
 });
 
+app.get("/polish", (req, res) => {
+  res.send("Czesc, Witamy w aplikacji!");
+});
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
